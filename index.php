adae0f5cf385f8d5675cfcf9e85ec7c22f9eecc0
EsmaeilRich <157278691+EsmaeilRich@users.noreply.github.com>
2024-01-26 02:20:56 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		VIP | ویرای گیم
+	</title>
+
+	<style>
+	* { font-family: verdana; font-size: 10pt; COLOR: gray; }
+	b { font-weight: bold; }
+	table { height: 50%; border: 1px solid gray;}
+	td { text-align: center; padding: 25;}
+
+	</style>
+</head>
+<body>
+<center>
+<br><br><br><br>
+	<table>
+	<tr><td>Welcome to the home of <b>dl.viraigame.ir</b></td></tr>
+	<tr><td>To change this page, upload your website into the public_html directory</td></tr>
+	<tr><td><img src="logo.png"></td></tr>
+	<tr><td style="font-size: 8pt">Date Created: <?php echo date("D M d H:i:s Y"); ?></td></tr>
+	</table>
+<br><br>
+
+</center>
+</body>
+</html>
